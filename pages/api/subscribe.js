becf69db1daeab88b0b65e43018a703b57b2b74a
DERIVE-studio <hello@derive.studio>
2026-05-11 19:53:25 +0200

    Update subscribe.js

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
       body: JSON.stringify({
         fields: {
           Email: email,
-          "Subscribed at": new Date().toISOString(),
+          "Subscribed at": new Date().toISOString().split("T")[0],
           Status: "Active",
         },
       }),
